4d27bb43c541d3a61fb929284c7c11e71d18ee27
wenli.wu <wenli.wu@horizon.ai>
2022-06-10 19:26:52 +0800

    fix(hobot_codec): [cr_id_skip] Add license no_3rd_skip
    Test Plan: verified
    
    Reviewers: kao.zhu, kairui.wang
    
    Reviewed By: kairui.wang
    
    Differential Revision: https://cr.hobot.cc/D184877

--- src/hobot_venc.cpp
+++ src/hobot_venc.cpp
@@ -1,8 +1,17 @@
-/***************************************************************************
- * COPYRIGHT NOTICE
- * Copyright 2020 Horizon Robotics, Inc.
- * All rights reserved.
- ***************************************************************************/
+// Copyright (c) 2022，Horizon Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "include/hobot_venc.h"
 // H264 H265 MJPEG
 #include "include/video_utils.hpp"
